6c28a527d067b5b6d6c930bd2a17b3326975d2f4
Chris Maes <maes@gurobi.com>
2015-10-19 20:38:30 -0400

    Add in scrollOffset option

--- jquery.nav.js
+++ jquery.nav.js
@@ -42,6 +42,7 @@
 			filter: '',
 			scrollSpeed: 750,
 			scrollThreshold: 0.5,
+                        scrollOffset: 0,
 			begin: false,
 			end: false,
 			scrollChange: false
@@ -202,7 +203,7 @@
 			var offset = $(target).offset().top;
 
 			$('html, body').animate({
-				scrollTop: offset
+				scrollTop: (offset - this.config.scrollOffset)
 			}, this.config.scrollSpeed, this.config.easing, callback);
 		},
 
